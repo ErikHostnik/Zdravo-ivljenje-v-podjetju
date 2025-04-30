c5ea8ebc5bacdab86c00632ecd87a6c6836840d8
Erik Hostnik <erik.hostnik@gmail.com>
2025-04-30 23:11:08 +0200

    ZDRAV-10 Created scrapper for weather data and fixed api.js connections

--- backend/api/api.js
+++ backend/api/api.js
@@ -1,25 +1,77 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const createError = require('http-errors');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-// Middleware
-app.use(express.json());
+// MongoDB connection URI
+const mongoDB = 'mongodb+srv://root:hojladrijadrom@zdravozivpodjetja.1hunr7p.mongodb.net/?retryWrites=true&w=majority&appName=ZdravoZivPodjetja';
 
-// Example route
-app.get('/', (req, res) => {
-  res.send('API is working!');
-});
-
-// MongoDB connection
-mongoose.connect('mongodb+srv://root:hojladrijadrom@zdravozivpodjetja.1hunr7p.mongodb.net/?retryWrites=true&w=majority&appName=ZdravoZivPodjetja', {
+// Connect to MongoDB
+mongoose.connect(mongoDB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
   console.log('MongoDB connected');
 }).catch(err => console.log(err));
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Session middleware
+app.use(session({
+  secret: 'shhhh-secret',
+  resave: true,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: mongoDB })
+}));
+
+// Set session to response locals
+app.use(function (req, res, next) {
+  res.locals.session = req.session;
+  next();
 });
+
+// Example root route
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
+
+// Routers
+const userRoutes = require('./routes/UserRoutes');
+const activityRoutes = require('./routes/ActivityRoutes');
+const routeRoutes = require('./routes/RouteRoutes');
+const weatherDataRoutes = require('./routes/WeatherDataRoutes');
+const challengeRoutes = require('./routes/ChallengeRoutes');
+
+// Use routers
+app.use('/api/users', userRoutes);
+app.use('/api/activities', activityRoutes);
+app.use('/api/routes', routeRoutes);
+app.use('/api/weatherdata', weatherDataRoutes);
+app.use('/api/challenges', challengeRoutes);
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404, 'Not Found'));
+});
+
+// Error handler - return JSON, not HTML
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
+});
+
+module.exports = app;
