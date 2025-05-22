41670f11f4d7ec48aa6444ac06c9e6cc51c992e0
Martin Jevševar <martin.jevsevar123@gmail.com>
2025-05-22 15:30:45 +0200

    ZDRAV-93 poravnava elementov v Home screen

--- frontend/app/mobile_app/lib/home.dart
+++ frontend/app/mobile_app/lib/home.dart
@@ -6,24 +6,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('FitOffice Domov')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, '/login'),
-              child: const Text('Prijava / Registracija'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, '/sensor'),
-              child: const Text('Spremljaj senzorje'),
-            ),
-          ],
+      appBar: AppBar(title: const Text('FitOffice Domov'), centerTitle: true),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, '/login'),
+                child: const Text('Prijava / Registracija'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, '/sensor'),
+                child: const Text('Spremljaj senzorje'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
