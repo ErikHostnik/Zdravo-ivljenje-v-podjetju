4e666dce690ce18672cd28b4f9ba379470779cdf
Martin Jevševar <martin.jevsevar123@gmail.com>
2025-05-26 17:31:07 +0200

    ZDRAV-100 uporaba knjižnice za razdelitev slik 80%-20%

--- backend/scripts/face_recognition/recognition_model.py
+++ backend/scripts/face_recognition/recognition_model.py
@@ -1,9 +1,35 @@
 import cv2
 import os
 import numpy as np
+from sklearn.model_selection import train_test_split
+
+# Nastavitve
+DATA_DIRS = ["data_preprocessed", "data_augmented"]  # Prilagodite
+TEST_SIZE = 0.2  # 20% za testiranje
 
 # Priprava podatkov
-data_dir = 'data_faces'  # mapa z imeniki za vsako osebo, znotraj slike .jpg/.png
-label_map = {}           # slovar: ime osebe -> številska oznaka
+label_map = {}
 faces, labels = [], []
 label_counter = 0
+
+for data_dir in DATA_DIRS:
+    for person_name in os.listdir(data_dir):
+        person_dir = os.path.join(data_dir, person_name)
+        if not os.path.isdir(person_dir):
+            continue
+        
+        # Dodaj osebo v label_map
+        if person_name not in label_map:
+            label_map[person_name] = label_counter
+            label_counter += 1
+        
+        # Naloži slike
+        for img_name in os.listdir(person_dir):
+            img_path = os.path.join(person_dir, img_name)
+            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+            if img is None:
+                print(f"Napaka pri nalaganju: {img_path}")
+                continue
+            
+            faces.append(img)
+            labels.append(label_map[person_name])
