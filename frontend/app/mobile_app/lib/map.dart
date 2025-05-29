717f64ad09479f9803c94bfb733a250fd921c2da
Martin Jevševar <martin.jevsevar123@gmail.com>
2025-05-29 15:40:06 +0200

    ZDRAV-105 prilagoditev velikosti zemljevida

--- frontend/app/mobile_app/lib/map.dart
+++ frontend/app/mobile_app/lib/map.dart
@@ -11,93 +11,39 @@ class SensorMapPage extends StatefulWidget {
   State<SensorMapPage> createState() => _SensorMapPageState();
 }
 
-class _SensorMapPageState extends State<SensorMapPage> with SingleTickerProviderStateMixin {
+class _SensorMapPageState extends State<SensorMapPage> {
   final mapController = MapController();
   final LatLng _fallbackCenter = const LatLng(46.5547, 15.6466);
   final double _defaultZoom = 15.0;
-
-  late final AnimationController _animController;
-  late Animation<double> _latAnim;
-  late Animation<double> _lngAnim;
-  late Animation<double> _zoomAnim;
-
-  LatLng _currentCenter;
-  double _currentZoom;
-
-  _SensorMapPageState()
-      : _currentCenter = const LatLng(46.5547, 15.6466),
-        _currentZoom = 15.0;
-
-  @override
-  void initState() {
-    super.initState();
-    _animController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 500),
-    )..addListener(_onAnimate);
-  }
 
   @override
   void didUpdateWidget(covariant SensorMapPage oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (widget.pathPoints != oldWidget.pathPoints && widget.pathPoints.isNotEmpty) {
-      _animateCameraTo(widget.pathPoints.last, _defaultZoom);
+      mapController.move(widget.pathPoints.last, _defaultZoom);
     }
-  }
-
-  void _animateCameraTo(LatLng targetCenter, double targetZoom) {
-    _animController.reset();
-
-    _latAnim = Tween<double>(begin: _currentCenter.latitude, end: targetCenter.latitude)
-        .animate(CurvedAnimation(parent: _animController, curve: Curves.easeInOut));
-    _lngAnim = Tween<double>(begin: _currentCenter.longitude, end: targetCenter.longitude)
-        .animate(CurvedAnimation(parent: _animController, curve: Curves.easeInOut));
-    _zoomAnim = Tween<double>(begin: _currentZoom, end: targetZoom)
-        .animate(CurvedAnimation(parent: _animController, curve: Curves.easeInOut));
-
-    _animController.forward().whenComplete(() {
-      _currentCenter = targetCenter;
-      _currentZoom = targetZoom;
-    });
-  }
-
-  void _onAnimate() {
-    final lat = _latAnim.value;
-    final lng = _lngAnim.value;
-    final zoom = _zoomAnim.value;
-    mapController.move(LatLng(lat, lng), zoom);
-  }
-
-  @override
-  void dispose() {
-    _animController.removeListener(_onAnimate);
-    _animController.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300, // Določi višino karte
+      height: 300,
       child: Stack(
         children: [
           FlutterMap(
             mapController: mapController,
             options: MapOptions(
-              // Če ni še nobene točke, gremo na rezervno lokacijo
               center: widget.pathPoints.isNotEmpty
                   ? widget.pathPoints.last
                   : _fallbackCenter,
               zoom: _defaultZoom,
-              interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate, 
-              // onemogočiš rotacijo, če želiš
+              interactiveFlags: InteractiveFlag.drag | InteractiveFlag.pinchZoom,
             ),
             children: [
               TileLayer(
                 urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                 userAgentPackageName: 'com.example.app',
               ),
-              // Če želiš narisati polilinijo tudi v ozadju
               if (widget.pathPoints.length > 1)
                 PolylineLayer(
                   polylines: [
